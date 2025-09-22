8652704961760d797d63b1550f99f08f53593540
aini1802 <nurainibintiabu@gmail.com>
2025-09-23 01:25:20 +0700

    Tugas Minggu 1

---
+++ project 1/verify_installation.py
@@ -0,0 +1,8 @@
+python
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+print("Semua library berhasil di-import!")
+print("Versi Numpy:", np.__version__)
+print("Versi Pandas:", pd.__version__)
